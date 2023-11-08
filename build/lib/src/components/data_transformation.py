ff8325fd9e7ead83df588ae2524a771bdb73712b
shahin28jan <126434583+shahin28jan@users.noreply.github.com>
2023-11-08 12:30:56 +0530

    Add files via upload

---
+++ build/lib/src/components/data_transformation.py
@@ -0,0 +1,120 @@
+import sys
+from dataclasses import dataclass
+import numpy as np 
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder,StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+import os
+from src.utils import save_object
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+    def get_data_transformation_object(self):
+        try:
+            logging.info('Data Transformation initiated')
+                       
+            numeric_column=['Age','Subscription_Length_Months',	'Monthly_Bill',	'Total_Usage_GB']
+            categorical_column=['Name', 'Location']
+
+            Gender=['Male', 'Female']
+            Location=['Los Angeles', 'New York', 'Miami', 'Chicago', 'Houston']
+            
+                      
+            logging.info('Pipeline Initiated')
+
+            ## Numerical Pipeline
+            num_pipeline=Pipeline(
+                steps=[
+                ('imputer',SimpleImputer(strategy='median')),
+                ('scaler',StandardScaler())
+
+                ]
+
+            )
+
+            # Categorigal Pipeline
+            cat_pipeline=Pipeline(
+                steps=[
+                ('imputer',SimpleImputer(strategy='most_frequent')),
+                ('ordinalencoder',OrdinalEncoder(categories=[Gender, Location])),
+                ('scaler',StandardScaler())
+                ]
+
+            )
+
+            preprocessor=ColumnTransformer([
+            ('num_pipeline',num_pipeline,numeric_column),
+            ('cat_pipeline',cat_pipeline,categorical_column)
+            ])
+
+            logging.info('Pipeline Completed')
+
+        except Exception as e:
+            logging.info("Error in Data Trnasformation")
+            raise CustomException(e,sys)
+        
+    def initaite_data_transformation(self,train_path,test_path):
+        try:
+            # Reading train and test data
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info('Read train and test data completed')
+            logging.info(f'Train Dataframe Head : \n{train_df.head().to_string()}')
+            logging.info(f'Test Dataframe Head  : \n{test_df.head().to_string()}')
+
+            logging.info('Obtaining preprocessing object')
+
+            preprocessing_obj = self.get_data_transformation_object()
+
+            target_column_name = 'Churn'
+            drop_columns = [target_column_name]
+            
+            
+            input_feature_train_df = train_df.drop(columns=drop_columns,axis=1)
+            target_feature_train_df=train_df[target_column_name]
+
+            input_feature_test_df=test_df.drop(columns=drop_columns,axis=1)
+            target_feature_test_df=test_df[target_column_name]
+
+                       
+            ## Trnasformating using preprocessor obj
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+
+            
+            logging.info("Applying preprocessing object on training and testing datasets.")
+            
+
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            save_object(
+
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+
+            )
+            logging.info('Preprocessor pickle file saved')
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+            
+        except Exception as e:
+            logging.info("Exception occured in the initiate_datatransformation")
+
+            raise CustomException(e,sys)
---
+++ build/lib/src/pipeline/predict_pipeline.py
@@ -0,0 +1,66 @@
+import sys
+import os
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import load_object
+import pandas as pd
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        try:
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            model_path=os.path.join('artifacts','model.pkl')
+
+            preprocessor=load_object(preprocessor_path)
+            model=load_object(model_path)
+
+            data_scaled=preprocessor.transform(features)
+            
+
+            pred=model.predict(data_scaled)
+            return pred
+            
+
+        except Exception as e:
+            logging.info("Exception occured in prediction")
+            raise CustomException(e,sys)
+        
+class CustomData:
+    def __init__(self,
+                 Age:float,
+                 Gender:str,
+                 Location:str,
+                 Subscription_Length_Months:float,
+                 Monthly_Bill:float,
+                 Total_UsAge_GB:float):
+        
+        self.Age=Age
+        self.Gender=Gender
+        self.Location=Location
+        self.Subscription_Length_Months=Subscription_Length_Months
+        self.Monthly_Bill=Monthly_Bill
+        self.Total_UsAge_GB=Total_UsAge_GB
+                
+    def get_data_as_dataframe(self):
+        
+        try:
+            custom_data_input_dict = {
+                'Age':[self.Age],
+                'Gender':[self.Gender],
+                'Location':[self.Location],
+                'Subscription_Length_Months':[self.Subscription_Length_Months],
+                'Monthly_Bill':[self.Monthly_Bill],
+                'Total_UsAge_GB':[self.Total_UsAge_GB]
+
+            }
+            df = pd.DataFrame(custom_data_input_dict)
+            logging.info('Dataframe Gathered')
+            return df
+            
+        except Exception as e:
+            logging.info('Exception Occured in prediction pipeline')
+            raise CustomException(e,sys)
